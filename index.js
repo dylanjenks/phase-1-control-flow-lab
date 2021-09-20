d88a1b0976e5fc62500d685e7880846c3359753b
dylanjenks <dylan.jenks92@gmail.com>
2021-09-20 14:20:33 -0400

    commit tree d891d822649f84846e16684a4b106e1da6ea723f

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(rideDistance){
   // Write your code here!
+  let result
+  if (rideDistance <= 400) {
+    result = 'This one is on me!';
+  } else if (rideDistance > 2000 && rideDistance < 2500) {
+    result = 'I will gladly take your thirty bucks.'
+  } else if (rideDistance > 2500) {
+    result = 'No can do.'
+  }
+  return result
+} 
+
+function ternaryCheckCity(rideCity){
+  // Write your code here!
+  let result
+  rideCity === 'NYC' ? (result = "Ok, sounds good.") : (result = "No go.")
+  return result
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(rideTip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  let result
+  switch (rideTip) {
+    case 'generous':
+      result = 'Thank you so much.'
+      return result
+      break;
+    case 'not as generous':
+      result = 'Thank you.'
+      return result
+      break;
+    case 'thanks for everything':
+      result = "Bye."
+      return result
+      break;
+  }
 }
